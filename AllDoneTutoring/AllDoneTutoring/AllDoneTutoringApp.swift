46e5090d6578a009e01a504dafef0470dd788cde
raonarvokshi <raonar.vokshi@gmail.com>
2024-02-20 21:39:02 +0100

    Setup Project Done

---
+++ AllDoneTutoring/AllDoneTutoring/AllDoneTutoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneTutoringApp.swift
+//  AllDoneTutoring
+//
+//  Created by Raonar Vokshi on 20.2.24.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneTutoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
